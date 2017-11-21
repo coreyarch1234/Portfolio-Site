bd6bde29f66e63912c0f38cf8592a12ba0bdfb38
coreyarch1234 <harrilal@mit.edu>
2017-11-21 12:48:16 -0800

    index js add

---
+++ themeforest-20826058-martin-luther-personal-portfolio-template/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Setting templating engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || port, function() {
+    console.log('server started: ' + port);
+    console.log('env port' + process.env.PORT);
+});
