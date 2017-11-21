3854fb1862d07b1b10a31e04633d3f12053cb268
coreyarch1234 <harrilal@mit.edu>
2017-11-21 14:16:09 -0800

    static files

--- themeforest-20826058-martin-luther-personal-portfolio-template/index.js
+++ themeforest-20826058-martin-luther-personal-portfolio-template/index.js
@@ -5,15 +5,20 @@ const port = 3000;
 
 
 //body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
 
 // Setting templating engine
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.use(express.static('public'));
+// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// app.set('view engine', 'handlebars');
+app.use(express.static('theme'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
 
 app.listen(process.env.PORT || port, function() {
     console.log('server started: ' + port);
